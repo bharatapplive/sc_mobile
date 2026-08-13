8c9aae93a1ccff239996350c58dc7ae60c3ec658
himanshu <himanshu9084r@gmail.com>
2026-08-13 12:33:24 +0530

    Initial commit

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -4,12 +4,37 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
+  standalone:false
 })
 export class LoginPage implements OnInit {
 
+  loginPortal = {identity:'', password:''};
+  registerPortal = {fullname:'', email:'', phone:'', password:''};
+  verifyPortal = {otp:''};
+  isLogin = true;
+  step: 'REGISTER' | 'OTP' = 'REGISTER';
   constructor() { }
 
   ngOnInit() {
   }
 
+  OnLoginHandler(form: any){
+
+  }
+
+  onRegisterHandler(form: any){
+    this.step = 'OTP';
+  }
+
+  onVerifyHandler(form: any){
+    this.isLogin = !this.isLogin;
+  }
+
+  toggleAuth(){
+    this.isLogin = !this.isLogin;
+  }
+
+  cancelOtp(){
+    this.step = 'REGISTER';
+  }
 }
