9d77a38db735485f0de85a4763dc332d716595bd
Sneha Singh <sneha.singh.1745@gmail.com>
2026-08-15 19:45:14 +0530

    Add registration page and login navigation

---
+++ src/app/register/register.page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.page.html',
+  styleUrls: ['./register.page.scss'],
+  standalone: false
+})
+export class RegisterPage {}
