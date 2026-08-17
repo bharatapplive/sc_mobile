765f8acf1cdf82e6ce5a054f32860ec546c99ee4
ajay shankar <ajay.shankar084@gmail.com>
2026-08-18 01:42:22 +0530

    feed changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,17 +4,18 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./home/home.module').then((m) => m.HomePageModule),
   },
   {
     path: '',
     redirectTo: 'login',
-    pathMatch: 'full'
+    pathMatch: 'full',
   },
   {
     path: 'login',
-    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+    loadChildren: () => import('./login/login.module').then((m) => m.LoginPageModule),
   },
+  // registeration routes
 ];
 
 @NgModule({
--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -2,10 +2,31 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePage } from './home.page';
 
-const routes: Routes = [
+export const routes: Routes = [
   {
     path: '',
     component: HomePage,
+    children: [
+      {
+        path: '',
+        redirectTo: 'feed',
+        pathMatch: 'full',
+      },
+      {
+        path: 'feed',
+        loadChildren: () =>
+          import('../feed/feed.module').then((m) => m.FeedPageModule),
+      },
+      {
+        path: 'profile',
+        loadChildren: () =>
+          import('../profile/profile.module').then((m) => m.ProfilePageModule),
+      }
+
+      // notofication route
+      // setting route
+      // search route
+    ],
   }
 ];
 
