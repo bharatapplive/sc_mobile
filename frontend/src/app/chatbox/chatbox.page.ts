c0cef8a00e2711d70aa6184b3f63b11942e712b3
himanshu <himanshu9084r@gmail.com>
2026-08-19 13:20:00 +0530

    Initial commit

--- frontend/src/app/chatbox/chatbox.page.ts
+++ frontend/src/app/chatbox/chatbox.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth-service';
 
 @Component({
   selector: 'app-chatbox',
@@ -7,10 +8,41 @@ import { Component, OnInit } from '@angular/core';
   standalone: false
 })
 export class ChatboxPage implements OnInit {
+  
+  avatarUrl?: string = '';
+  selectedTag: string = 'Primary';
 
-  constructor() { }
+  tags: string[] = ['Primary', 'Requests', 'General'];
 
-  ngOnInit() {
+  constructor(
+    private readonly authServe: AuthService
+  ) { }
+
+  ngOnInit() { 
+    this.authServe.loadUserData().subscribe({
+      next: (userData) => {
+        this.avatarUrl = userData.avatarUrl?.trim();       
+      },
+      error: (err) => {
+        console.error('Failed to load user profile:', err);
+      },
+    });
   }
 
+  getUserAvatar(): string{
+
+      if (this.avatarUrl) {
+      // Return absolute URLs directly
+      if (this.avatarUrl.startsWith('http://') || this.avatarUrl.startsWith('https://')) {
+        return this.avatarUrl;
+      }
+    }
+
+    // Default fallback placeholder
+    return 'assets/images/default-avatar.png';
+  }
+
+  selectTag(tag: string) {
+    this.selectedTag = tag;
+  }
 }
